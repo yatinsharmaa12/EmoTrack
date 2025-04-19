949603e9cd69da0f1c23e4bfa35c63e022f478f3
orionpax12 <aman1312sharma@gmail.com>
2025-04-19 16:29:07 +0530

    Added utils

---
+++ utils/hash.ts
@@ -0,0 +1,35 @@
+import { createCipheriv, createDecipheriv } from 'crypto'
+
+// The below one is the algorithm we are using to encrypt
+const algorithm = 'aes-256-cbc'
+
+// Taking the Key and iv values from the environment variables
+const keyHex = process.env.KEY 
+const ivHex = process.env.IV 
+
+if(!keyHex || !ivHex) {
+  throw new Error('Key or IV Environment variables are missing')
+}
+
+// converting the key and iv values to buffer values
+const key = Buffer.from(keyHex, 'hex')
+const iv = Buffer.from(ivHex, 'hex')
+
+// The below function is used to encrypt the value you pass to it
+const encryptPassword = (password: string): string => {
+  const cipher = createCipheriv(algorithm, key, iv)
+  let encrypted = cipher.update(password, 'utf8', 'hex')
+  encrypted += cipher.final('hex')
+  return encrypted
+}
+
+// The below function is used to decrypt the encrypted value passed to it
+const decryptPassword = (encryptedPassword: string): string => {
+  const decipher = createDecipheriv(algorithm, key, iv)
+  let decrypted = decipher.update(encryptedPassword, 'hex', 'utf8')
+  decrypted += decipher.final('utf8')
+  return decrypted
+}
+
+// exporting the functions so that they can be used in another files
+export { encryptPassword, decryptPassword }
